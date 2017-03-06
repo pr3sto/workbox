a3a1a5a1e4a7f2da1111682b7252f9253bc72cfd
pr3sto <pr3sto1377@gmail.com>
2017-03-06 20:27:03 +0300

    Finish install scripts

--- project/scripts/install_software.sh
+++ project/scripts/install_software.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
-apt-get update
+/usr/bin/apt-get update
 
 # apache
-apt-get -y install apache2 libapache2-mod-wsgi 
-
-# mongodb
-apt-get -y install mongodb
+/usr/bin/apt-get -y install apache2 libapache2-mod-wsgi 
 
 # vagrant
-apt-get -y install vagrant
-
-# docker
-apt-get -y install docker.io
-ln -sf /usr/bin/docker.io /usr/local/bin/docker
-sed -i '$acomplete -F _docker docker' /etc/bash_completion.d/docker.io
-
-# autostart at boot
-update-rc.d apache2 defaults
-update-rc.d docker.io defaults
-update-rc.d mongod defaults
+/usr/bin/apt-get -y install vagrant
---
+++ project/scripts/setup_mongo_schema.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+RED='\033[0;31m'
+NC='\033[0m'
+
+# service mongod should be started
+if /bin/ps ax | /bin/grep -v /bin/grep | /bin/grep mongod > /dev/null
+then
+
+  # setup workbox
+  . /opt/venvs/workbox/bin/activate
+  cd /opt/venvs/workbox
+  /opt/venvs/workbox/bin/gearbox setup-app -c /opt/venvs/workbox/lib/python2.7/site-packages/workbox/wsgiconfig/production.ini
+  deactivate
+
+else
+
+  /usr/bin/printf "\n${RED}Error:${NC} service mongod not started\n\n"
+  exit 1
+
+fi
+
