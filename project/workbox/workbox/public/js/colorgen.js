d24bc13b3f04e62b0e601a28edcfd77970ffb108
pr3sto <pr3sto1377@gmail.com>
2017-03-08 18:54:14 +0300

    add createbox page; improve style; separate js code

---
+++ project/workbox/workbox/public/js/colorgen.js
@@ -0,0 +1,45 @@
+/***
+colorgen.js
+
+generate colors background
+***/
+
+function gen5colors_bkg() {
+    var randomColor1 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor2 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor3 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor4 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor5 = '#' + Math.random().toString(16).slice(-6);
+
+    return 'white linear-gradient(-90deg,' +
+        randomColor1 + ' 20%,' +
+        randomColor2 + ' 20%, ' + randomColor2 + ' 40%,' +
+        randomColor3 + ' 40%, ' + randomColor3 + ' 60%,' +
+        randomColor4 + ' 60%, ' + randomColor4 + ' 80%,' +
+        randomColor5 + ' 80%) no-repeat scroll 0px 100% / 100% 2px';
+}
+
+function gen10colors_border() {
+    var randomColor1 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor2 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor3 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor4 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor5 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor6 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor7 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor8 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor9 = '#' + Math.random().toString(16).slice(-6);
+    var randomColor10 = '#' + Math.random().toString(16).slice(-6);
+
+    return 'linear-gradient(-90deg,' +
+        randomColor1 + ' 10%,' +
+        randomColor2 + ' 10%, ' + randomColor2 + ' 20%,' +
+        randomColor3 + ' 20%, ' + randomColor3 + ' 30%,' +
+        randomColor4 + ' 30%, ' + randomColor4 + ' 40%,' +
+        randomColor5 + ' 40%, ' + randomColor5 + ' 50%,' +
+        randomColor6 + ' 50%, ' + randomColor6 + ' 60%,' +
+        randomColor7 + ' 60%, ' + randomColor7 + ' 70%,' +
+        randomColor8 + ' 70%, ' + randomColor8 + ' 80%,' +
+        randomColor9 + ' 80%, ' + randomColor9 + ' 90%,' +
+        randomColor10 + ' 90%) 2';
+}
---
+++ project/workbox/workbox/public/js/smooth-goto-href.js
@@ -0,0 +1,22 @@
+/***
+smooth-goto-href.js
+
+smooth slide to href on page
+***/
+
+$(function () {
+    $('a[href*="#"]:not([href="#"])').click(function () {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')) {
+            if (location.hostname == this.hostname) {
+                var target = $(this.hash);
+                target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+                if (target.length) {
+                    $('html, body').animate({
+                        scrollTop: target.offset().top - 50
+                    }, 1000);
+                    return false;
+                }
+            }
+        }
+    });
+});
