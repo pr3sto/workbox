17a627caff8466c001a1cccfc64c1f8b4bb761ff
pr3sto <pr3sto1377@gmail.com>
2017-09-15 23:52:05 +0300

    delete examples, move from project to root

--- test/HelloMongo/hellomongo/model/session.py
+++
@@ -1,5 +0,0 @@
-from ming import Session
-from ming.odm import ThreadLocalODMSession
-
-mainsession = Session()
-DBSession = ThreadLocalODMSession(mainsession)
--- test/HelloMongo/hellomongo/websetup/schema.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Setup the HelloMongo application"""
-from __future__ import print_function
-
-from tg import config
-
-
-def setup_schema(command, conf, vars):
-    """Place any commands to setup hellomongo here"""
